9262c22c5d313f3eac1bf03795d4969c1b4896bf
pandaachuidc <53323186+pandaachuidc@users.noreply.github.com>
2020-07-16 16:08:41 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,15 @@
+
+
+$(".s2__facialSlider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  // slidesToScroll: 1,
+  autoplay: true,
+  // autoplaySpeed: 1000,
+  adaptiveHeight: true,
+  arrows: false,
+});
+
+
